5f644ee6583f343dd874c0f760c4f89b0a21f9dc
Kwan Holloway <kwansholloway@gmail.com>
2018-05-19 15:13:07 -0400

    Basic routing

---
+++ aws-fitbit/src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: 'home',
+    redirectTo: '',
+    loadChildren: 'app/home-page/home-page.module#HomePageModule',
+  },
+  {
+    path: 'events',
+    loadChildren: 'app/events-page/events-page.module#EventsPageModule'
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- aws-fitbit/src/app/app.module.ts
+++ aws-fitbit/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { EventsPageComponent } from './events-page/events-page.component';
+import { HomePageModule } from './home-page/home-page.module';
+import { EventsPageModule } from './events-page/events-page.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomePageComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomePageModule,
+    EventsPageModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
