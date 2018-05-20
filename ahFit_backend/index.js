29c3c5ecf49185f5c7eca371a04e927e241ae4cf
stuart <themastrgamr@gmail.com>
2018-05-20 01:55:19 -0400

    shot dummy data

--- ahFit_backend/index.js
+++ ahFit_backend/index.js
@@ -27,8 +27,41 @@ app.post('/eventData', function (req, res) {
   console.log('TODO: do something with this req.body: ', req.body)
 })
 
+app.get('/events', function (req, res) {
+  const result = {
+    "events": [
+      {
+        "name": "Adidas 5K",
+        "description": "Compete in this event to win $1,000 worth of Ethereum. Along with a pair of Limited Edition Yeezy Boost 350 v2 Zebra Stripe Edition!",
+        "start_date": "2018-05-19T15:12:17.000",
+        "end_date": "2018-05-26T15:12:17.000",
+        "admission_fee": 30,
+        "current_pool_amount": 1000,
+        "participants": []
+      },
+      {
+        "name": "J.P. Morgan Swim Freestyle",
+        "description": "Compete to be the fastest swimmer in this freestyle swimming event!",
+        "start_date": "2018-05-24T15:12:17.000",
+        "end_date": "2018-05-31T15:12:17.000",
+        "admission_fee": 50,
+        "current_pool_amount": 5000,
+        "participants": []
+      },
+      {
+        "name": "Tour De NYC, B",
+        "description": "Do you got what it takes to lap around Manhattan on your Bicycle? Sign up here, and we checkin fax, B.",
+        "start_date": "2018-05-24T15:12:17.000",
+        "end_date": "2018-05-31T15:12:17.000",
+        "admission_fee": 25,
+        "current_pool_amount": 1500,
+        "participants": []
+      }
+    ]
+  }
+  res.send(result)
+})
+
 app.listen(3000, function () {
   console.log('App listening on port 3000!')
 })
-
-curl -i -X "{"geo":}" http://localhost:3000/eventData
